4ece220c91f86f08dcebf6055f6e511770f202cc
jvc-github <50381053+jvc-github@users.noreply.github.com>
2022-10-04 12:53:48 -0300

    Creación del Servicio para obtener datos

--- portfolio/src/app/components/navbar/navbar.component.ts
+++ portfolio/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  /* Servicio inyectado en el componente */
+  constructor(private datos:PortfolioService) { }
+
 
   ngOnInit(): void {
+    /* Acceso al servicio */
+    this.datos.obtenerDatos();
   }
 
 }
